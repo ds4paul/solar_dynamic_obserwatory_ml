7736d37497ec15227fa5e4d16d4f8f5bc17ddeda
ds4paul <pauljozefczyk@gmail.com>
2025-11-17 17:46:30 +0100

    update data links

---
+++ src/00_download_data.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+import gdown
+
+# Setup project directories
+
+BASE_DIR = Path(__file__).resolve().parents[1]
+RAW_DIR = BASE_DIR / "data" / "raw"
+RAW_DIR.mkdir(parents=True, exist_ok=True)
+
